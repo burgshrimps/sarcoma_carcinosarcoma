0281fc9649986401181d6bed3c975dadea7387b1
alan27 <alan27@mi.fu-berlin.de>
2020-01-18 16:03:21 +0100

    cox reg test + primarius analysis

--- analysis.py
+++ analysis.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
-#from lifelines import KaplanMeierFitter
-from sklearn.linear_model import LinearRegression
+#from lifelines import KaplanMeierFitter, CoxPHFitter
+#from lifelines.datasets import load_rossi
 import seaborn as sns
 
 
@@ -41,10 +41,10 @@ def adjacent_values(vals, q1, q3):
     lower_adjacent_value = np.clip(lower_adjacent_value, vals[0], q1)
     return lower_adjacent_value, upper_adjacent_value
 
-def plot_violin(df, feature, ylabel):
-    carc = df[feature][(df.carcinosarcoma == 1)]
-    sar = df[feature][(df.carcinosarcoma == 0)]
-    data = [np.array(sorted(carc.dropna())), np.array(sorted(sar.dropna()))]
+def plot_violin(df, feature, cat, ylabel, xticklabels):
+    cat1 = df[feature][(df[cat] == 1)]
+    cat2 = df[feature][(df[cat] == 0)]
+    data = [np.array(sorted(cat1.dropna())), np.array(sorted(cat2.dropna()))]
     parts = plt.violinplot(data, positions=[1,2], showextrema=False)
 
     for pc in parts['bodies']:
@@ -58,7 +58,7 @@ def plot_violin(df, feature, ylabel):
     plt.vlines(inds, quartile1, quartile3, color='k', linestyle='-', lw=5)
     plt.vlines(inds, whiskersMin, whiskersMax, color='k', linestyle='-', lw=1)
     
-    plt.xticks([1,2], ['Karzinosarkom', 'Sarkom'])
+    plt.xticks([1,2], [xticklabels[0], xticklabels[1]])
     plt.ylabel(ylabel)
     plt.show()
 
@@ -144,7 +144,6 @@ col_names = ['id_old', 'id_new', 'hospital', 'date_birth', 'age_diagnosis', 'dat
 df = pd.read_csv('Daten.csv', sep=',', usecols=list(range(58)), names=col_names, skiprows=1)
 pd.set_option('display.max_rows', df.shape[0]+1)
 
-
 # Clean data: dates
 df = df[:-3].drop(['id_old', 'date_birth'], axis=1)
 df.hospital[df.index[df.hospital == 'klinikum GT']] = 'Klinikum GT'
@@ -169,7 +168,7 @@ df = df.drop(['follow_up_a', 'follow_up_b', 'follow_up_c'], axis=1)
 df['death_observed'] = df.date_death.notna() * 1
 df['last_obs'] = df.date_death
 df.last_obs[pd.notna(df.last_follow_up)] = df.last_follow_up[pd.notna(df.last_follow_up)]
-df['duration'] = 12 * (df.last_obs.dt.year - df.date_diagnosis.dt.year) + (df.last_obs.dt.month - df.date_diagnosis.dt.month)
+df.loc[:, 'duration'] = 12 * (df.last_obs.dt.year - df.date_diagnosis.dt.year) + (df.last_obs.dt.month - df.date_diagnosis.dt.month)
 df.duration[76] = np.nan  # exclude from analysis because date of death unclear
 df.death_observed[76] = 0
 
@@ -183,7 +182,6 @@ df.hypercholesterolemia = df.hypercholesterolemia.map(d_bin_cat)
 df.uterus_myomatosus = df.uterus_myomatosus.map(d_bin_cat)
 df.hrt_contraceptive = df.hrt_contraceptive.str.strip().map(d_hrt_contra)
 df.endometriosis = df.endometriosis.map(d_bin_cat)
-
 
 
 # Clean data: multiple categories
@@ -203,6 +201,28 @@ d_tumor_type = {'Leiomyosarkom Corpus' : 'Leiomyosarkom',
 d_figo = {'IV' : 4, 'IVB' : 4, 'IIIC' : 3, 'II' : 2, 'Ib' : 1, 'Ia' : 1, 'IIIc' : 3, 'III' : 3, 'IIIa' : 3, 'I' : 1, 'IIb' : 2, 'IB' : 1, 
           'IVa' : 4, 'IIa' : 2, 'IIB' : 2, 'Ivb' : 4, 'Ic' : 1, 'IIIb' : 3, 'Iib' : 2, 'IIIB' : 3, 'Iva' : 4, 'IIIA' : 3, 'IA' : 1, 
           'Iia' : 2, 'IVA' : 4, 'IIC' : 2, 'IIc' : 2, 'IVb' : 4, 'IC' : 1}
+d_T = {'pT1b' : 1, 'pT2b' : 2, 'pT3c' : 3, 'pT3b' : 3, 'pT1a' : 1, 'pT2' : 2, 'pT1c' : 1, 'pT3a' : 3, 'pT2c' : 2, 'pT2a' : 2, 'pT3' : 3,
+       'pT4' : 4, 'pT1' : 1, 'pt2' : 2, 'ypT3c' : 3}
+d_M = {'M1' : 1, 'M0' : 0, 'pM1' : 1}
+d_L = {'L0' : 0, 'L1' : 1}
+d_primarius = {'10 cm' : 10.0, '2,5 cm' : 2.5, '6 cm' : 6.0, '4 cm' : 4.0, '10cm' : 10.0, '8 cm' : 8.0, '7 cm' : 7.0, '4.5 cm' : 4.5,
+               '13 cm' : 13.0, '3 cm' : 3.0, '100x100x40' : 10.0, '120x100x80' : 12.0, '10x7x7cm' : 10.0, '10x14 cm' : 14.0, 
+               '55mm' : 5.5, '6.5 cm' : 6.5, '160mm' : 16.0, '160 x 70 x 50 mm' : 16.0, '115' : 11.5, '110x100x80 mm' : 11.0, 
+               '6,5cm' : 6.5, '4,2x1,2cm' : 4.2, 'max Tumorgröße 59mm' : 5.9, '40 x 45 x  53 mm' : 4.0, '195x130x75mm' : 19.5, 
+               '12x9cm' : 12.0, '60 x  55 x 55 mm' : 6.0, '230 x 200 x 110 mm' : 23.0, '120 x 70 x 40 mm' : 12.0, '130x100x100 mm' : 13.0,
+               '18 cm' : 18.0, '195x175x105mm' : 19.5, '55x65x100 mm' : 5.5, '75x80x60 cm' : 8.0, '15 cm' : 15.0,
+               'mind. 90 mm Durchmesser' : 9.0, '90x70x55mm' : 9.0, '4,3x4x4cm' : 4.3, '19 mm Durchmesser' : 1.9, '50 x 40 x 35 mm' : 5.0,
+               '85x60x45 mm' : 8.5, '220 x 110 x 80 mm' : 22.0, '47mm' : 4.7, '9cm' : 9.0, 'Einzelne Knoten bis 5 cm' : 5.0, 
+               '5,5x7,5x8,5cm' : 8.5, '3,8 cm' : 3.8, '250 mm Durchmesser' : 25.0, '130x120x100mm' : 13.0, '80' : 8.0, '130' : 13.0, 
+               '7,5 cm' : 7.5, '190x130x100 mm' : 19.0, '0,4 bis 1,6' : 1.6, '6-7cm' : 7.0, '125x105x30' : 12.5, '4,2 cm' : 4.2, 
+               '100x90x90' : 10.0, '60x60x50' : 6.0, '16 cm' : 16.0, '220x80x70 mm' : 22.0, '70x50x30mm' : 7.0, '56' : 5.6,
+               '200x100x40mm' : 20.0, '160x130x90mm' : 16.0, '4x5cm' : 4.0, '120x70x20mm' : 12.0, '8x4x3' : 8.0, '5.5 cm' : 5.5,
+               '40x27x30 mm' : 4.0, '100 x 90 x 70 mm' : 10.0, '45x40x27' : 4.5, '65x60x50 mm' : 6.5, '14x8x6,5 cm' : 14.0,
+               '45x48x35mm' : 4.5, '17,5x13,5x13' : 17.5, '410x200x90mm' : 41.0, '>5cm (Tumor morcelliert)' : 5.0, '70x60x70' : 7.0,
+               '100x90x60 mm' : 10.0, '4x3 cm' : 4.0, '320 x 195 x 130 mm' : 32.0, '33mm Durchmesser' : 3.3, '210x140x50mm' : 21.0,
+               '12 cm' : 12.0, '11 cm' : 11.0, '64 x 46 x 30mm' : 6.4, '3x4 cm' : 3.0, '35 x 37 x ca. 38 mm.' : 3.8, '48x38mm' : 4.8,
+               '35mm' : 3.5, '6x10x12cm' : 12.0, '6cm' : 6.0, '5x3,5cm' : 5.0, '2 cm' : 2.0, '10 x 14 x 11 cm' : 14.0, 
+               '290x270bis30' : 29.0, '35x65x45 mm' : 6.5, '40x35x25mm' : 4.0, '170x120x120 mm' : 17.0, '3,5 cm' : 3.5, '9 cm' : 9.0}
 df.tumor_type = df.tumor_type.str.strip()
 df.tumor_type = df.tumor_type.map(d_tumor_type)
 kar_uterus = (df.tumor_type == 'Karzinosarkom Uterus')
@@ -212,6 +232,18 @@ sar_adeno = (df.tumor_type == 'Adenosarkom')
 sar_ess = (df.tumor_type == 'Endometriales Stromasarkom')
 df.figo = df.figo.str.strip()
 df.figo = df.figo.map(d_figo)
+df['T'] = df['T'].str.strip()
+df['T'] = df['T'].map(d_T)
+df['N'] = df['N'].str.split(' ')
+df['N'] = df['N'].dropna().map(lambda x: float(x[0][-1]))
+df['M'] = df['M'].map(d_M)
+df['G'] = df['G'].dropna().map(lambda x: float(x[1]))
+df['R'] = df['R'].dropna().map(lambda x: float(x[1]))
+df['V'] = df['V'].dropna().map(lambda x: float(x[1]))
+df['L'] = df['L'].map(d_L)
+df['Pn'] = df['Pn'].dropna().map(lambda x: float(x[2]))
+df['size_primarius'] = df['size_primarius'].str.strip()
+df['size_primarius'] = df['size_primarius'].map(d_primarius)
 
 
 # Set value of patient with carcinosarcoma to 1
@@ -243,7 +275,7 @@ if do_kmf:
 do_age_diagnosis = False
 if do_age_diagnosis:
     plot_hist(df, 'age_diagnosis', 'Alter bei Diagnose', 'Anzahl Patienten')
-    plot_violin(df, 'age_diagnosis', 'Alter bei Diagnose')
+    plot_violin(df, 'age_diagnosis', 'carcinosarcoma', 'Alter bei Diagnose', ['Karzinosarkom', 'Sarkom'])
     print('Median Age', np.median(df.age_diagnosis.dropna()))
 
 
@@ -251,7 +283,7 @@ if do_age_diagnosis:
 do_bmi = False
 if do_bmi:
     plot_hist(df, 'bmi', 'BMI', 'Anzahl Patienten')
-    plot_violin(df, 'bmi', 'BMI')
+    plot_violin(df, 'bmi', 'carcinosarcoma', 'BMI', ['Karzinosarkom', 'Sarkom'])
     print('Median BMI', np.median(df.bmi.dropna()))
 
 
@@ -275,7 +307,14 @@ if do_para:
     xticklabels = np.array_str(np.arange(9))[1:-1].split(' ')
     plt.xticks(np.arange(9)+0.5, xticklabels)
     plt.show()
-    plot_violin(df, 'para', 'Parität')
+    plot_violin(df, 'para', 'carcinosarcoma', 'Parität', ['Karzinosarkom', 'Sarkom'])
+
+# Analysis: Size Primarius
+do_primarius = True
+if do_primarius:
+    plot_hist(df, 'size_primarius', 'Größe Primarius [cm]', 'Anzahl Patienten')
+    plot_violin(df, 'size_primarius', 'carcinosarcoma', 'Größe Primarius [cm]', ['Karzinosarkom', 'Sarkom'])
+    print('Median Primarius Size', np.median(df.size_primarius.dropna()))
 
 
 # Analysis: FIGO and Age
@@ -311,12 +350,49 @@ if do_um_figo:
 do_recc = False
 if do_recc:
     plot_hist(df, 'duration_recurrance', 'Dauer bis rezidiv [Monate]', 'Anzahl Patienten')
-    plot_violin(df, 'duration_recurrance', 'Dauer bis rezidiv [Monate]')
+    plot_violin(df, 'duration_recurance', 'carcinosarcoma', 'Dauer bis rezidiv [Monate]', ['Karzinosarkom', 'Sarkom'])
 
-do_age_recc = True
+do_age_recc = False
 if do_age_recc:
     plot_reg(df, 'age_diagnosis', 'duration_recurrance', 'Alter bei Diagnose', 'Dauer bis rezidiv [Monate]')
 
-do_bmi_recc = True
+do_bmi_recc = False
 if do_bmi_recc:
     plot_reg(df, 'bmi', 'duration_recurrance', 'BMI', 'Dauer bis rezidiv [Monate]')
+
+do_hypertension_recc = False
+if do_hypertension_recc:
+    plot_violin(df, 'duration_recurrance', 'hypertension', 'Dauer bis rezidiv [Monate]', ['Hypertonie ja', 'Hypertonie nein'])
+
+do_diabetes_recc = False
+if do_diabetes_recc:
+    plot_violin(df, 'duration_recurrance', 'diabetes', 'Dauer bis rezidiv [Monate]', ['Diabetes ja', 'Diabetes nein'])
+
+do_hypercholesterolemia_recc = False
+if do_hypercholesterolemia_recc:
+    plot_violin(df, 'duration_recurrance', 'hypercholesterolemia', 'Dauer bis rezidiv [Monate]', ['Hypercholestrinämie ja', 'Hypercholestrinämie nein'])
+
+do_figo_recc = False
+if do_figo_recc:
+    plot_violin_figo(df, 'duration_recurrance', 'Dauer bis rezidiv [Monate]')
+
+do_primarius_recc = False
+if do_primarius_recc:
+    plot_reg(df, 'size_primarius', 'duration_recurrance', 'Größe Primarius [cm]', 'Dauer bis rezidiv [Monate]')
+
+
+#print(df.age_diagnosis.median())
+"""
+cph = CoxPHFitter()
+dur_not_na_idx = df.duration.notna()
+df_cph = df[dur_not_na_idx]
+df_cph.age_diagnosis = df_cph.age_diagnosis.fillna(df_cph.age_diagnosis.median())
+df_cph.bmi = df_cph.bmi.fillna(df_cph.bmi.median())
+#print(pd.isnull(df_cph[['duration', 'death_observed', 'age_diagnosis', 'bmi']]))
+cph.fit(df_cph[['duration', 'death_observed', 'age_diagnosis', 'bmi']], duration_col='duration', event_col='death_observed')
+print(cph.summary)
+"""
+
+#print(df[['duration_recurrance', 'hypertension']].corr())
+
+#print(df['size_primarius'].value_counts())
